8c507b3d61bdb63fa505ed1b7a1ecfffc5a26acc
Maarten Kwant <mkwant@xs4all.nl>
2023-02-23 21:49:09 +0100

    BuyeeMercari: Added SOLD text to description

--- auction_extractors/buyee_mercari.py
+++ auction_extractors/buyee_mercari.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from auction_extractors.base import AuctionExtractor
 from models import AuctionSearchResponse, Auction
 
-# TODO Add if sold: <div class="soldOut__text">SOLD</div>
+
 # TODO Translate
 
 
@@ -18,20 +18,25 @@ class BuyeeMercari(AuctionExtractor):
         params = {'keyword': self.search_term, 'status': 'all'}
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0'}
         page = requests.get(url=url, params=params, headers=headers)
-
         soup = BeautifulSoup(page.content, 'html.parser')
+
         auction_list = soup.find('ul', {'class': 'item-lists'})
         auctions_source = auction_list.findAll('li', {'class': 'list'})
         auctions = []
 
         for auction in auctions_source:
+
             title = auction.find('h2', {'class': 'name'}).text
             link = f"https://buyee.jp{auction.find('a')['href'].split('?')[0]}"
             auction_id = link.split('/')[-1]
             image_link = f"https://static.mercdn.net/item/detail/orig/photos/{auction_id}_1.jpg"
             _price_yen = auction.find('p', {'class': 'price'}).text
             _price_eur = auction.find('p', {'class': 'price-fx'}).text
-            description = f'{_price_yen} {_price_eur}'
+            _sold_out = auction.find('div', {'class': 'soldOut__text'})
+            if _sold_out:
+                description = f'{_sold_out.text} - {_price_yen} {_price_eur}'
+            else:
+                description = f'{_price_yen} {_price_eur}'
 
             auctions.append(Auction(title=title,
                                     auction_id=auction_id,
@@ -44,3 +49,7 @@ class BuyeeMercari(AuctionExtractor):
                                      search_term=self.search_term,
                                      site_desc='Buyee (Mercari)',
                                      auctions=auctions)
+
+
+bm = BuyeeMercari(search_term='bowie')
+bm.search()
