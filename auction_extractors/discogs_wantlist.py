01a93512118b319c41549f393abc137cdca4f19b
Maarten Kwant <mkwant@xs4all.nl>
2023-03-27 23:47:36 +0200

    Removed todo (not possible, no 'updated' in html version of offer page)

--- auction_extractors/discogs_wantlist.py
+++ auction_extractors/discogs_wantlist.py
@@ -10,9 +10,6 @@ from auction_extractors.base import AuctionExtractorAsync
 from models import AuctionSearchResponse, Auction
 
 
-# TODO scrape HTML marketplace page instead of using the xml version - get item image, shipping costs a.o.
-
-
 class DiscogsWantlist(AuctionExtractorAsync):
     search_term: str
     headers = {
