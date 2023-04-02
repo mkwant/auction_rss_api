22a9eaafad7b0ff891f6eaf24bc4b0f9beb6b04d
Maarten Kwant <mkwant@xs4all.nl>
2023-04-02 21:57:09 +0200

    Removed test code

--- dependencies/translate.py
+++ dependencies/translate.py
@@ -28,24 +28,3 @@ async def translate_text(client: httpx.AsyncClient,
     r = await client.post(url, headers=headers, json=payload)
     result = r.json()[0]['translations'][0]['text']
     return result
-
-#
-# def main():
-#     text_jpn = ['戦場のメリークリスマス 4K修復版 大規模ロードショー パンフレット Ryuichi Sakamoto 坂本龍一 DAVID BOWIE デヴィッドボウイ 大島渚 12',
-#                 '[MT][S060060] 未使用 デビッド・ボウイ 東芝EMI特製紙ジャケ収納ボックス※DU特典ではありません',
-#                 'IGGY POP イギーポップ The Idiot 帯付 国内盤◎中古/再生未確認/ノークレームで/盤面スレ汚れ/PUNK/プロデュース DAVID BOWIE',
-#                 '戦場のメリークリスマス 4K修復版 B2(約73×51㎝)アート ポスターB デヴィッド・ボウイ DAVID BOWIE 大島渚 坂本龍一 チラシ付 未使用',
-#                 'デビッドボウイ EP チェンジス',
-#                 'ビギナーズ / David Bowie ep '
-#                 ]
-#
-#     client = httpx.AsyncClient()
-#
-#     for i in text_jpn:
-#
-#         result = await translate_text(client=client, text=i, from_language='ja', to_language='en')
-#
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
