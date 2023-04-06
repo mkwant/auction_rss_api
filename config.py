1ede48a03b9bec43579d943aa34d5c2f2ef1d652
Maarten Kwant <mkwant@xs4all.nl>
2023-04-06 18:27:51 +0200

    Changed ms keys to .env

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     ebay_app_id: str
-    ms_translate_api_key: str = "0da11c9863204e69a0a743ae54094702"
-    ms_translate_api_location: str = "westeurope"
+    ms_translate_api_key: str
+    ms_translate_api_location: str
 
     class Config:
         env_file = ".env"
