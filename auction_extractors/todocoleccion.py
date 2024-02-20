42f50f7b14cfff516d70df87c17cb0612e4b7ab6
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-02-20 11:08:55 +0100

    Simplified selectors

--- auction_extractors/todocoleccion.py
+++ auction_extractors/todocoleccion.py
@@ -18,8 +18,8 @@ class Todocoleccion(AuctionExtractor):
         scraper = cloudscraper.create_scraper()
 
         r = scraper.get(self.URL, params=params)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        site_auctions = soup.find_all('div', {'class': '_lote_item-image-and-content'})
+        soup = BeautifulSoup(r.content, features='html.parser')
+        site_auctions = soup.select('div._lote_item-image-and-content')
         return site_auctions
 
     def search(self) -> AuctionSearchResponse:
@@ -27,26 +27,26 @@ class Todocoleccion(AuctionExtractor):
         auctions = []
 
         for auction in self._get_auctions(search_term=self.search_term):
-            image_link = auction.find('img')['src'].split('?')[0]
+            image_link = auction.select_one('img')['src'].split('?')[0]
 
-            _item_info = auction.find('a', {'class': 'ga-track-click-in-lote_item-link js-lot-titles block'})
+            _item_info = auction.select_one('a.js-lot-titles')
             title = _item_info.text.strip()
             link = 'https://en.todocoleccion.net' + _item_info['href']
             unique_id = _item_info['id'].replace('lot-title-', '')
-            price = auction.find('span', {'class': 'text-nowrap precio-lote-listado'}).text
+            price = auction.select_one('span.precio-lote-listado').text
 
-            _seller_block = auction.find('p', {'class': 'lote-vendedor'})
+            _seller_block = auction.select_one('p.lote-vendedor')
             if _seller_block is not None:
-                seller = _seller_block.find('span').text.strip()
+                seller = _seller_block.select_one('span').text.strip()
             else:
                 seller = ''
 
-            _item_type_block = auction.find('a', {'class': '_lote_item-img-footerbox'})
+            _item_type_block = auction.select_one('a._lote_item-img-footerbox')
             if _item_type_block is not None:
                 _item_type = _item_type_block.text.strip()
             else:
                 _item_type = 'Buy now'
-            _category = auction.find('p', {'class': '_lote_item-section'}).text.strip()
+            _category = auction.select_one('p._lote_item-section').text.strip()
             desc = f'{price} | {_item_type} | {_category}'
 
             auctions.append(Auction(auction_id=unique_id,
