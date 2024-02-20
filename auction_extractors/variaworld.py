a55c2188711c9914c2c6b6ada8fdaba39ea1e013
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-02-20 11:42:27 +0100

    Simplified selectors

--- auction_extractors/variaworld.py
+++ auction_extractors/variaworld.py
@@ -14,9 +14,9 @@ class Variaworld(AuctionExtractor):
     def _get_auctions(self) -> ResultSet:
         URL = f'https://www.variaworld.nl/alles/m_ge=[j;m;u]&ts=1&zoek={self.search_term}&zoek_at=a&m_sr=lig&startpagina=1'  # noqa
         r = requests.get(URL)
-        soup = BeautifulSoup(r.content, 'html.parser')
+        soup = BeautifulSoup(r.content, features='html.parser')
 
-        site_auctions = soup.find_all('a', {'class': 'overzichtbox_2'})
+        site_auctions = soup.select('a.overzichtbox_2')
         return site_auctions
 
     def search(self) -> AuctionSearchResponse:
@@ -25,19 +25,19 @@ class Variaworld(AuctionExtractor):
         for auction in self._get_auctions():
             link = auction['href']
             auction_id = link.split('&at=')[1].split('&')[0]
-            image = auction.find('div', {'class': 'overzichtfotobox_2'})
-            image_link = f"https://www.variaworld.nl{image.find('img')['src']}"
-            _artist_name = auction.find('div', {'class': 'koptekst'}).text.strip()
-            _item_name, _item_type = auction.find_all('div', {'class': 'tekst'})
+            image = auction.select_one('div.overzichtfotobox_2')
+            image_link = f"https://www.variaworld.nl{image.select_one('img')['src']}"
+            _artist_name = auction.select_one('div.koptekst').text.strip()
+            _item_name, _item_type = auction.select('div.tekst')
             _item_name = _item_name.text.strip()
             _item_type = _item_type.text.strip()
             try:
-                _item_price = auction.find('span', {'class': 'div_kleur_prijs_1'}).text.strip()
+                _item_price = auction.select_one('span.div_kleur_prijs_1').text.strip()
             except AttributeError:
-                _item_price = auction.find('span', {'class': 'div_kleur_prijs_2'}).text.strip()
+                _item_price = auction.select_one('span.div_kleur_prijs_2').text.strip()
             title = f"{_artist_name} - '{_item_name}' ({_item_type})"
             description = '\n'.join([_artist_name, _item_name, _item_type, _item_price])
-            start_date = auction.find('div', {'class', 'overzicht_datum_ingebracht'}).text.strip()
+            start_date = auction.select_one('div.overzicht_datum_ingebracht').text.strip()
             start_date = datetime.strptime(start_date, '%d-%m-%Y')
 
             auctions.append(Auction(auction_id=auction_id,
