4eb6f71faf4547430d3a30ebdd76cfed0889e2c9
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-02-27 14:24:29 +0100

    Simplified middleware

--- app/middleware.py
+++ app/middleware.py
@@ -1,12 +1,9 @@
-from fastapi import Request
+from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 
 
 class AddNoIndex(BaseHTTPMiddleware):
-    def __init__(self, app):
-        super().__init__(app)
-
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(self, request: Request, call_next) -> Response:
         response = await call_next(request)
         response.headers["x-robots-tag"] = 'noindex, nofollow'
 
