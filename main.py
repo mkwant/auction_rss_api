d33711c35c15c118da9ef1b091af28c3b42498a6
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-04-15 11:32:06 +0200

    Bumped version

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from routers import redirect, rss
 # Instantiate FastApi
 app = FastAPI(
     title='AuctionRSS',
-    version='1.8.9',
+    version='1.8.10',
     description='This API returns RSS feeds for the search results of (mostly) auction sites.'
 )
 
