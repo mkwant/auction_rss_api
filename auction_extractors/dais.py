1680353341782a835a3de6d97d2fe237b1baf0f2
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-09-03 15:17:00 +0200

    Fixed bug

--- auction_extractors/dais.py
+++ auction_extractors/dais.py
@@ -35,7 +35,7 @@ class Dais(AuctionExtractor):
             # If search_in_desc, search in description as well
             if self.search_in_desc:
                 if (not self.search_term.lower() in product['vendor'].lower() or
-                        self.search_term.lower() in product['body_html'].lower()):
+                        not self.search_term.lower() in product['body_html'].lower()):
                     continue
             if not self.search_in_desc:
                 if not self.search_term.lower() in product['vendor'].lower():
