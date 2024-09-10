0f90f274fb53186590a37c46b50121f6e060f276
Maarten Kwant <maarten.kwant@nepgroup.com>
2024-09-10 13:03:24 +0200

    Made an abstract Shopify AuctionExtractor, and made Dais AuctionExtractor inherit from that.

--- auction_extractors/dais.py
+++ auction_extractors/dais.py
@@ -1,63 +1,11 @@
-import json
-from typing import List
-
-import dateparser
-import requests
-
-from models.auction import Auction
-from models.auctionextractor import AuctionExtractor
+from models.auctionextractor_shopify import ShopifyExtractor
 
 
-class Dais(AuctionExtractor):
-    search_in_desc: bool = False
-
-    @property
-    def search_link(self) -> str:
-        return f"https://www.daisrecords.com/search?q={self.search_term}"
-
+class Dais(ShopifyExtractor):
     @property
     def site_desc(self) -> str:
         return "Dais"
 
-    def get_auctions(self) -> List[Auction]:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0'
-        }
-
-        auctions = []
-
-        url = 'https://www.daisrecords.com/products.json?limit=250'
-        r = requests.get(url=url, headers=headers)
-
-        products = json.loads(r.text)['products']
-        for product in products:
-
-            # If search_in_desc, search in description as well
-            if self.search_in_desc:
-                if (self.search_term.lower() not in product['vendor'].lower() and
-                        self.search_term.lower() not in product['body_html'].lower()):
-                    continue
-            if not self.search_in_desc:
-                if self.search_term.lower() not in product['vendor'].lower():
-                    continue
-
-            title = f"{product['vendor']} - {product['title']}"
-            auction_id = str(product['id'])
-            link = 'https://www.daisrecords.com/products/' + product['handle']
-            image_link = product['images'][0]['src']
-            start_date = dateparser.parse(product['created_at'])
-
-            _variants = '\n'.join([f"${x['price']} - {x['title']}" for x in product['variants']])
-            description = f"{_variants}\n\n{product['body_html']}"
-
-            auctions.append(
-                Auction(**{
-                    'title': title,
-                    'auction_id': auction_id,
-                    'description': description,
-                    'link': link,
-                    'image_link': image_link,
-                    'start_date': start_date
-                }))
-
-        return auctions
+    @property
+    def domain(self):
+        return 'daisrecords.com'
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from routers import redirect, rss
 # Instantiate FastApi
 app = FastAPI(
     title='AuctionRSS',
-    version='1.12.4',
+    version='1.12.5',
     description='This API returns RSS feeds for the search results of (mostly) auction sites.'
 )
 
