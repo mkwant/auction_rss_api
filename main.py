426c40f5d18e1f50114c99a952025e2141951749
Maarten Kwant <maarten.kwant@nepgroup.com>
2025-02-12 10:53:39 +0100

    Fixed feed: changed size variable

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 # Instantiate FastApi
 app = FastAPI(
     title='AuctionRSS',
-    version='1.16.14',
+    version='1.16.15',
     description='This API returns RSS feeds for the search results of auction sites, '
                 'online record stores and concert venues.'
 )
