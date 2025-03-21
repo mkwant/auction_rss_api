cbdfb45f32e471390a7616cbda5a40e59d898142
Maarten Kwant <maarten.kwant@nepgroup.com>
2025-03-21 17:17:32 +0100

    Added a Greedbag abstract auctionextractor

---
+++ models/auctionextractor_greedbag.py
@@ -0,0 +1,56 @@
+import hashlib
+from abc import ABC, abstractmethod
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+from models.auction import Auction
+from models.auctionextractor import AuctionExtractor
+
+
+class GreedbagExtractor(AuctionExtractor, ABC):
+    """A base class for Greedbag sites."""
+
+    @property
+    @abstractmethod
+    def site_name(self) -> str:
+        """The name of the Greedbag site, i.e 'soisong' for 'soisong.greedbag.com'."""
+        ...
+
+    @property
+    def search_link(self) -> str:
+        return f'https://{self.site_name}.greedbag.com'
+
+    @property
+    def site_desc(self) -> str:
+        return f'{self.site_name}.greedbag.com'
+
+    def get_auctions(self) -> List[Auction]:
+        auctions = []
+
+        r = requests.get(self.search_link)
+        soup = BeautifulSoup(r.content, features="html.parser")
+
+        products = soup.select('div.hproduct')
+        for product in products:
+            _title = product.select_one('h2.album').text.strip()
+            _artist = product.select_one('h3.artist').text.strip()
+            title = f'{_artist} - {_title}'
+            link = self.search_link + product.select_one('a')['href']
+            image_link = product.select_one('img')['src']
+            _desc = product.select_one('div.description').text.strip()
+            _price = product.select_one('div.line-details').text.strip()
+            description = f"{_price}\n\n{_desc}"
+            auction_id = hashlib.md5(link.encode('utf-8')).hexdigest()
+
+            auctions.append(
+                Auction(**{
+                    'title': title,
+                    'auction_id': auction_id,
+                    'description': description,
+                    'link': link,
+                    'image_link': image_link,
+                }))
+
+        return auctions
