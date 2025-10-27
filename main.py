06422bfc621d89c0c5a0feb70f36ecb389a4332c
Kwant, M. <M.Kwant@rechtspraak.nl>
2025-10-27 16:38:16 +0100

    Bumped version

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 # Instantiate FastApi
 app = FastAPI(
     title='AuctionRSS',
-    version='1.19.4',
+    version='1.19.5',
     description='This API returns RSS feeds for the search results of auction sites, '
                 'online record stores and concert venues.'
 )
