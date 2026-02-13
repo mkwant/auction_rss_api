fd4de4fdb96e867f6612f1e618847c19daf839f0
Kwant, M. <M.Kwant@rechtspraak.nl>
2026-02-13 17:21:19 +0100

    Added feed: AudiophileUSA

--- routers/recordshops.py
+++ routers/recordshops.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from fastapi_rss import RSSResponse
 
 from auction_extractors.anonne import Anonne
+from auction_extractors.audiophileusa import AudiophileUSA
 from auction_extractors.backstage import BackStage
 from auction_extractors.bandcamp import Bandcamp
 from auction_extractors.bandcamp_faves import BandcampFaves
@@ -62,6 +63,12 @@ router = APIRouter(
 @router.get(path='/anonne')
 def anonne_rss() -> RSSResponse:
     site = Anonne()
+    return site.search()
+
+
+@router.get(path='/audiophileusa')
+def audiophileusa_rss(search_term: str) -> RSSResponse:
+    site = AudiophileUSA(search_term=search_term)
     return site.search()
 
 
