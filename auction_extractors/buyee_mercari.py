032ce14feaa91f93a12f79f10b3e72cbf9b6fefc
Kwant, M. <M.Kwant@rechtspraak.nl>
2026-03-16 12:31:12 +0100

    Fixed feed: fixed extraction

--- auction_extractors/buyee_mercari.py
+++ auction_extractors/buyee_mercari.py
@@ -1,4 +1,3 @@
-import json
 from typing import List
 
 import httpx
@@ -9,64 +8,57 @@ from models.auctionextractor import AuctionExtractor
 
 
 class BuyeeMercari(AuctionExtractor):
-    search_term: str
+
+    @property
+    def search_link(self) -> str:
+        return f"https://buyee.jp/mercari/search?limit=100&lang=en&page=1&searchType=filter&order-sort=desc-created_time&keyword={self.search_term}&currencyCode=EUR"
 
     @property
     def site_desc(self) -> str:
         return 'Buyee (Mercari)'
 
-    @property
-    def search_link(self) -> str:
-        return (f'https://buyee.jp/mercari/search?keyword={self.search_term}'
-                f'&status=all&items=40&lang=en&currencyCode=EUR')
-
-    def _get_page(self) -> str:
-        """Retrieve search page."""
-        url = 'https://asf.myeeglobal.com/mercari'
-        params = {'keyword': self.search_term,
-                  'status': 'all',
-                  'conversionType': 'Mercari_DirectSearch',
-                  'currencyCode': 'EUR',
-                  'myee': 0,
-                  'languageCode': 'en',
-                  'lang': 'en'
-                  }
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0'
-        }
-        client = httpx.Client(headers=headers)
-        r = client.get(url=url, params=params)
-        r.raise_for_status()
-        return r.text
-
     def get_auctions(self) -> List[Auction]:
-        """Parse search page."""
-        soup = BeautifulSoup(self._get_page(), features='html.parser')
-        json_string = soup.select_one('script#__NEXT_DATA__').contents[0]
-        parsed_json = json.loads(str(json_string))
-        auction_list = parsed_json['props']['pageProps']['catalog']['entries']
         auctions = []
 
-        for auction in auction_list:
-            title = auction['names']['ja']
-            auction_id = auction['item']['code']
-            link = f"https://buyee.jp/mercari/item/{auction_id}"
-            image_link = f"https://static.mercdn.net/item/detail/orig/photos/{auction_id}_1.jpg"
-            _price_yen = f"{auction['price']['value']:,} yen"
-            _price_eur = f"€{auction['localPrice']['value']:.2f}"
-            if not auction['hasStock']:
-                description = f'SOLD - {_price_yen} ({_price_eur})'
-            else:
-                description = f'{_price_yen} ({_price_eur})'
-            seller = auction['store']['names']['ja']
+        link = 'https://buyee.jp/mercari/search'
+        params = {
+            'limit': 100,
+            'keyword': self.search_term,
+            'order-sort': 'desc-created_time',
+            'currencyCode': 'EUR',
+        }
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0'}
+
+        r = httpx.get(url=link, params=params, headers=headers)
+        r.raise_for_status()
+        soup = BeautifulSoup(markup=r.text, features='html.parser')
+
+        items = soup.select('ul.item-lists>li.list')
+        for item in items:
+            link = 'https://buyee.jp' + item.select_one('a')['href']
+            auction_id = link.split('?')[0].split('/')[-1]
+            _image_link = item.select_one('img')['data-bind'].replace('\n', ' ').split('\'')[1].replace('/small/',
+                                                                                                        '/large/')
+            image_link = f"https:{_image_link.split('?')[0]}"
+
+            title = item.select_one('h2.name').text.strip()
+
+            _price_yen = item.select_one('p.price').text.strip()
+            _price_eur = item.select_one('p.price-fx').text.strip()
+            try:
+                _sold_out = item.select_one('div.soldOut__text').text.strip()
+                description = f"{_sold_out}\n{_price_yen} {_price_eur}"
+            except AttributeError:
+                description = f"{_price_yen} {_price_eur}"
 
             auctions.append(
-                Auction(**{
-                    'title': title,
-                    'auction_id': auction_id,
-                    'description': description,
-                    'link': link,
-                    'image_link': image_link,
-                    'seller': seller
-                }))
+                Auction(
+                    auction_id=auction_id,
+                    link=link,
+                    image_link=image_link,
+                    title=title,
+                    description=description,
+                )
+            )
+
         return auctions
