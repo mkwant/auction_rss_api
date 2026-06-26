916dcb8ef45cda7b07732bf4b16eac92d408cf46
Maarten Kwant <mkwant@xs4all.nl>
2026-06-26 11:45:05 +0200

    fix: Tradera: Fix extraction of json

--- src/auction_rss_api/auction_extractors/tradera.py
+++ src/auction_rss_api/auction_extractors/tradera.py
@@ -1,9 +1,10 @@
+import codecs
 import json
+import re
 from typing import List, Literal
 
 import dateutil
-import requests
-from bs4 import BeautifulSoup
+import httpx
 
 from auction_rss_api.models.auction import Auction
 from auction_rss_api.models.auctionextractor import AuctionExtractor
@@ -21,36 +22,134 @@ class Tradera(AuctionExtractor):
     def site_desc(self) -> str:
         return 'Tradera'
 
+    @staticmethod
+    def extract_flight_payloads(html: str) -> list[str]:
+        pushes = re.findall(
+            pattern=r"self\.__next_f\.push\(\[(.*?)\]\)",
+            string=html,
+            flags=re.S,
+        )
+
+        decoded_payloads = []
+
+        for p in pushes:
+            first_quote = p.find('"')
+            last_quote = p.rfind('"')
+
+            if first_quote == -1 or last_quote == -1:
+                continue
+
+            raw = p[first_quote + 1:last_quote]
+
+            try:
+                decoded = codecs.decode(raw, "unicode_escape")
+            except Exception:
+                continue
+
+            decoded_payloads.append(decoded)
+
+        return decoded_payloads
+
+    @staticmethod
+    def extract_currencies(payloads: list[str]) -> list[dict]:
+        for payload in payloads:
+            key = '"currencies":'
+            idx = payload.find(key)
+            if idx == -1:
+                continue
+
+            array_start = payload.find('[', idx)
+            if array_start == -1:
+                continue
+
+            bracket_count = 0
+            for i in range(array_start, len(payload)):
+                if payload[i] == '[':
+                    bracket_count += 1
+                elif payload[i] == ']':
+                    bracket_count -= 1
+                    if bracket_count == 0:
+                        array_end = i + 1
+                        array_str = payload[array_start:array_end]
+                        try:
+                            return json.loads(array_str)
+                        except json.JSONDecodeError as e:
+                            print(f"JSON decode error: {e}")
+                            continue
+        return []
+
+    @staticmethod
+    def extract_items(payloads: list[str]) -> list[dict]:
+        for payload in payloads:
+            if "discover/receiveSearchResults" in payload:
+                try:
+                    start = payload.find('"actions"')
+                    start = payload.rfind("{", 0, start)
+
+                    depth = 0
+                    end = None
+
+                    for i in range(start, len(payload)):
+                        if payload[i] == "{":
+                            depth += 1
+                        elif payload[i] == "}":
+                            depth -= 1
+                            if depth == 0:
+                                end = i + 1
+                                break
+
+                    obj = json.loads(payload[start:end])
+                    return obj["actions"][0]["payload"]["result"]["items"]
+
+                except Exception:
+                    continue
+
+        return []
+
     def _get_json_data(self) -> dict:
-        """Find the json string on the page and parse it."""
         url = 'https://www.tradera.com/en/search'
         params = {
             'q': self.search_term,
             'sortBy': 'AddedOn'
         }
+
         cookies = {
             'preferred_currency': self.currency,
-            'shipping_country': 'NL',  # Doesn't work, always shows shipping to Sweden...
+            'shipping_country': 'NL',
             'gdpr_consent_v1': '1:1,2:1,3:1,4:1',
         }
+
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0'
         }
 
-        r = requests.get(url=url, params=params, cookies=cookies, headers=headers)
-        soup = BeautifulSoup(r.text, features='html.parser')
+        r = httpx.get(
+            url=url,
+            params=params,
+            cookies=cookies,
+            headers=headers
+        )
+        r.raise_for_status()
 
-        json_str = soup.select_one(selector='script#__NEXT_DATA__', namespaces={'type': 'application/json'}).text
-        return json.loads(json_str)['props']['pageProps']['initialState']
+        payloads = self.extract_flight_payloads(r.text)
+
+        items = self.extract_items(payloads)
+        currencies = self.extract_currencies(payloads)
+
+        return {
+            "items": items,
+            "currencies": currencies
+        }
 
     def get_auctions(self) -> List[Auction]:
         data = self._get_json_data()
-        currency = data['multiCurrency']['preferredCurrency']
-        items = data['discover']['items']
+
+        currency_map = {c["code"]: c for c in data["currencies"]}
+        currency = currency_map[self.currency]
 
         auctions = []
 
-        for item in items:
+        for item in data["items"]:
             auction_id = str(item['itemId'])
             title = item['shortDescription']
             image_link = item['imageUrlTemplate'].replace('{format}', 'large-fit')
